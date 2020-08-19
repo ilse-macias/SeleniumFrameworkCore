4bad45722592a9e79d389c1a67d68974ecfd3e47
Ilse Macías <ilse.macias@gmail.com>
2020-08-19 07:45:12 -0700

    Remove now ignored directory

--- AutomationPracticeCore/POM/HomePage.cs
+++ AutomationPracticeCore/POM/HomePage.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using System.Threading;
+using OpenQA.Selenium;
+
+namespace AutomationPracticeCore.POM
+{
+    public class HomePage
+    {
+        public IWebDriver _driver;
+
+        private IWebElement signInButton;
+
+        private IWebElement emailAddressField;
+        private IWebElement passwordField;
+
+        public HomePage(IWebDriver _driver)
+        {
+            this._driver = _driver;
+        }
+
+        public void SignIn()
+        {
+            signInButton = _driver.FindElement(By.ClassName("login"));
+            signInButton.Click();
+            Thread.Sleep(3000);
+
+            emailAddressField = _driver.FindElement(By.Id("email"));
+            emailAddressField.SendKeys("demo@mailinator.com");
+
+            passwordField = _driver.FindElement(By.Id("passwd"));
+            passwordField.SendKeys("12345");
+
+        }
+       
+    }
+}
---
+++ AutomationPracticeCore/TestCases/Test.cs
@@ -0,0 +1,38 @@
+﻿using AutomationPracticeCore.POM;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace AutomationPracticeCore
+{
+    [TestFixture()]
+    public class Test
+    {
+
+        private IWebDriver _driver;
+
+        [SetUp()]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Cookies.DeleteAllCookies();
+            _driver.Manage().Window.Maximize();
+            _driver.Url = "http://automationpractice.com/index.php";
+        }
+
+        [Test()]
+        public void ValidateSignIn()
+        {
+            HomePage homePage = new HomePage(_driver);
+            homePage.SignIn();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Close();
+            _driver.Quit();
+        } 
+    }
+}
